6578ae968182a0a741d0508e9e437f39db36f82c
Ziming Yang <zyang@bnl.gov>
2021-08-11 23:45:02 -0400

    make one argument more explict

--- lib/clean/clean_test.py
+++ lib/clean/clean_test.py
@@ -13,7 +13,7 @@ class MyTestCase(unittest.TestCase):
         ground_truth_file = os.path.join(self.data_dir, 'ground_truth.tsv')
         df_test = pd.read_csv(test_file, sep="\t")
         df_ground_truth = pd.read_csv(ground_truth_file, sep="\t")
-        res = rows_and_cols_quant_filter(df_test, skip_cols=2, cutoff=4, pandas=True)
+        res = rows_and_cols_quant_filter(df_test, start_col_indexgi=2, cutoff=4, pandas=True)
         self.assertTrue(df_ground_truth.reset_index(drop=True).equals(res.reset_index(drop=True)))
 
 
